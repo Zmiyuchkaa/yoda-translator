4fc80c289b7d78f8d9323056455b320ac55e6a02
Elena Rolskaya <e.rolskaya@kv.beets>
2019-05-26 18:48:15 +0300

    finished main part

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 /*
 использование БЭМ
-js анимация
 вставить ссылки на tips
+сделать что-то с стилем historyRequests
+добавить комментарии к блокам
+прописать условие что если пустая строка - не пушай в историю поиска, и вообще пушай только если нажали на кнопку
 */
 
 
@@ -9,35 +11,43 @@ document.getElementById("submit").onclick = () => {
   let text = document.getElementById("fname").value;
   document.getElementById("main").innerHTML = "";
   document.getElementById("history").innerHTML = "";
+  document.getElementById("inspiration").innerHTML = "";
   axios.get(`https://cors-anywhere.herokuapp.com/http://yoda-api.appspot.com/api/v1/yodish?text=${text}`)
   .then(response => {
     let div = document.getElementById('main');
-    div.innerHTML = response.data.yodish;
+    let createResponse = document.createElement("h2")
+    let createTitle = document.createElement("p");
+    div.appendChild(createTitle);
+    div.appendChild(createResponse); 
+    createTitle.innerHTML = "Translated to Yodish:"
+    createResponse.innerHTML = response.data.yodish;
   })
+}
 
-  let addRequestToHistory = request => {
-    let historyRequests = JSON.parse(localStorage.getItem('history'));
-    if (historyRequests === null) {
-      localStorage.setItem('history', JSON.stringify([text]))
-    } else if (historyRequests.includes(text) == true) {
-      return;
-    } else {
+document.getElementById("button-history").onclick = () => {
+  let text = document.getElementById("fname").value;
+  let historyRequests = JSON.parse(localStorage.getItem('history')); 
+  if (historyRequests === null) {
+    localStorage.setItem('history', JSON.stringify([text]))
+  } else { 
       if (historyRequests.length > 5) {
         historyRequests.shift();
       }
-      historyRequests.push(request);
-      
-      localStorage.setItem('history', JSON.stringify(historyRequests));
-    }
-  let historyDiv = document.getElementById('history')
-  let historyP = document.createElement('p')
+    historyRequests.push(text);
+    localStorage.setItem('history', JSON.stringify(historyRequests));
+  }
+  let historyDiv = document.getElementById('history');
+  let historyTitle = document.createElement("h4");
+  let historyP = document.createElement('p');
+  historyTitle.className = "history__title";
   historyP.className = 'history__p'
   historyP.innerHTML = JSON.parse(localStorage.getItem('history'));
-  historyDiv.appendChild(historyP)
-  }
-  
-  addRequestToHistory(text);
-  }
+  historyTitle.innerHTML = (`That's the last things users were looking for:`);
+  historyDiv.appendChild(historyTitle);
+  historyDiv.appendChild(historyP);
+  document.getElementById("inspiration").style.display = "none";
+  document.getElementById("button-history").style.display = "none";
+}
 
   // Sets the number of stars we wish to display
 const numStars = 100;
